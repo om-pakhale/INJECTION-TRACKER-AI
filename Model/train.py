e51c634e4b5a3db5043206f2ded688715f930659
om-pakhale <ompakhale8975@gmail.com>
2026-08-12 23:03:44 +0530

    Little bit upgrade in model

--- Model/train.py
+++ Model/train.py
@@ -2,10 +2,13 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
 import joblib 
 from urllib.parse import urlparse , unquote
 def expert_normalize(text):
-    text = unquote(text)
+    if not isinstance(text , str):
+        return ""
+    text = unquote(text).replace("+"," ")
     
     if text.startswith(('http://', 'https://')):
         parsed = urlparse(text)
@@ -14,43 +17,27 @@ def expert_normalize(text):
     return text.lower()
 
 
-data =  pd.read_csv("Dataset/SQLInjection_XSS_CommandInjection_MixDataset.1.0.0.csv")
+data =  pd.read_csv("SQLInjection_XSS_CommandInjection_MixDataset.1.0.0.csv")
 df = pd.DataFrame(data)
 df['label'] = df[['SQLInjection', 'XSS', 'CommandInjection']].max(axis=1)
-
+df['Sentence']= df['Sentence'].apply(expert_normalize)
+X_train , X_test , Y_train , Y_test = train_test_split(
+    df['Sentence'], df['label'], random_state=42 , stratify=df['label']
+)
 vector = TfidfVectorizer(analyzer='char',ngram_range=(1, 3), max_features=20000,lowercase=True)
-benign_urls = [
-    "https://leetcode.com/problems/two-sum/",
-    "https://www.linkedin.com/feed/",
-    "https://github.com/trending",
-    "https://google.com/search?q=python+code",
-    "https://stackoverflow.com/questions",
-    "https://www.wikipedia.org/",
-    "/problems/two-sum/?",  
-    "/feed/?"               
-]
-
-df_benign = pd.DataFrame({"Sentence": benign_urls, "label": 0})
-
-df_final = pd.concat([df] + [df_benign] * 500, ignore_index=True)
-df_final['Sentence'] = df_final['Sentence'].apply(expert_normalize)
-X = vector.fit_transform(df_final['Sentence'].values.astype('U'))
-Y = df_final['label']
+X_train_normal =  vector.fit_transform(X_train.values.astype('U'))
+X_test_normal = vector.transform(X_test.values.astype('U'))
 
 model = RandomForestClassifier(n_estimators=100,n_jobs=-1,class_weight='balanced',verbose=1)
 print("Tranning Model...")
-model.fit(X,Y)
+model.fit(X_train_normal,Y_train)
 
-test_payload = ["<svg/onload=alert(1)>"]
-test_vector = vector.transform(test_payload)
-prediction = model.predict(test_vector)
+y_pred = model.predict(X_test_normal)
+print("\n--- Model Evaluation ---")
+print(classification_report(Y_test, y_pred))
 
 joblib.dump(model, 'full_injection_model.pkl')
 joblib.dump(vector, 'full_vectorizer.pkl')
-print(f"Is it malicious? {'YES' if prediction[0] == 1 else 'NO'}")
-
-for payload, pred in zip(test_payload, prediction):
-    status = "MALICIOUS" if pred == 1 else "CLEAN"
-    print(f"Payload: {payload} -> Result: {status}")
+print("Model updated")
 
 
